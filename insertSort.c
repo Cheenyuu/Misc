6fd194c22622cf9cbf508703ddd05f4fbb2d462d
Chenyu Zheng <chnyzheng@gmail.com>
2025-08-11 16:07:50 -0400

    8/11/2025'

--- insertSort.c
+++ insertSort.c
@@ -1,6 +1,35 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+
+static int N = 1000;
+
+int* initialize_array(int size){
+    int *arr =  malloc(sizeof(int) * size);
+    for(int i = 0; i < size; i++){
+        arr[i] = rand() % (N + 1);
+    }
+    return arr;
+}
+
+void print_array(int* arr, int size){
+    for(int i = 0; i < size; i++){
+        printf(" [%d] ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
+void insertSort(int* arr){
+
+
+
+
+}
+
 
 int main(){
-    printf("testing...");
+    int *array = initialize_array(5);
+    print_array(array, 5);
 }
